cde42db0f2aa1daf4487bb56fa2064165a74be5d
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-09-28 16:50:34 +1000

    added a navbar and connectedf the pages, need to style the navbar, and sort out a form for the contact page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import MainPage from "./MainPage";
 import Nav from "./Nav";
 import "./App.css";
@@ -8,9 +9,23 @@ import Work from "./Work";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <MainPage />
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <div className="navbar-container">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/work">Work</Link>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/work" component={Work} />
+          </div>
+        </Switch>
+      </BrowserRouter>
+      {/* <Nav />
+      <MainPage /> */}
     </div>
   );
 }
