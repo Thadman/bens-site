85d9808608379fd307fe212da776132e3926ceaa
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-09-29 13:31:11 +1000

    added some more stlying

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./images/bvd-black.jpg";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fa-arrow-down} from "@fortawesome/free-solid-svg-icons";
 
 export default function MainPage() {
   return (
@@ -8,11 +10,13 @@ export default function MainPage() {
       <div className="image-container">
         <div className="navbar">
           <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/work">Work</Link>
         </div>
         <img src={logo} alt="this is the logo" />
+      </div>
+      <div className="navigation-arrow">
+        <Link to="/about">About</Link>
       </div>
     </div>
   );
