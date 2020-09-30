c52bc9daf412a90578e738f4d3a782b895111153
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-09-30 17:27:15 +1000

    added bouncing arrow with react-animations

--- src/MainPage.js
+++ src/MainPage.js
@@ -5,10 +5,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import styled, { keyframes } from "styled-components";
 import { fadeInDown } from "react-animations";
+import { bounce } from "react-animations";
 
 const FadeInAnimation = keyframes`${fadeInDown}`;
 const FadeInDiv = styled.div`
   animation: 1 3s ${FadeInAnimation};
+`;
+
+const bounceAnimation = keyframes`${bounce}`;
+const BounceDiv = styled.div`
+  animation: 10s ${bounceAnimation};
 `;
 
 export default function MainPage() {
@@ -24,11 +30,13 @@ export default function MainPage() {
         <div className="image-container">
           <img src={logo} alt="this is the logo" />
         </div>
-        <div className="navigation-arrow">
-          <NavLink to="/about">
-            <FontAwesomeIcon icon={faArrowDown} size="3x" />
-          </NavLink>
-        </div>
+        <BounceDiv>
+          <div className="navigation-arrow">
+            <NavLink to="/about">
+              <FontAwesomeIcon icon={faArrowDown} size="3x" />
+            </NavLink>
+          </div>
+        </BounceDiv>
       </FadeInDiv>
     </div>
   );
